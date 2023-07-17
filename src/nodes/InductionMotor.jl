74b93a5e8c4a6a838b4fc5b201735b8b91116f31
CarlRe <74236322+CarlRe@users.noreply.github.com>
2023-07-17 15:06:12 +0200

    Induction Motor into /nodes
    still trying to somehow implement my own models to the Package without using IONodes

---
+++ src/nodes/InductionMotor.jl
@@ -0,0 +1,17 @@
+
+@DynamicNode InductionMotor(P,J,Tm,B,ωb) begin
+    MassMatrix()
+end begin
+    @assert P > 0
+    @assert J > 0
+    @assert Tm > 0 
+    @assert B > 0
+    @assert ωb > 0
+end [[ω, dω],[Te, dTe]] begin
+    TI = P/ω
+
+    dω = (Te - TI)/J
+    dTe = -(Te + Tm) - (B/ωb)*((ω - ωb)/Tm)
+end
+
+export  InductionMotor
